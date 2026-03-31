dca18453f5a1c2bdc1fa0713a438039169d24d78
Abraao Ylgner <abraaoayrs@gmail.com>
2026-03-31 19:50:43 -0300

    Inicialização do projeto da Pós, tech challenge, fase 1

---
+++ src/test/java/com/pos/tech/challenge/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.tech.challenge.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
